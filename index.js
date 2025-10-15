183d0f5747da6ea306ba6b5f8563b1932005d40f
Prosenjith Dash Pappu <prosenjith.edu@gmail.com>
2025-10-15 13:27:19 +0600

    RentWheels-BD Collection create to Database done

--- index.js
+++ index.js
@@ -21,12 +21,25 @@ const client = new MongoClient(uri, {
 });
 
 async function run() {
-  try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-      console.log("Pinged your deployment. You successfully RentWheels-BD connected to MongoDB!"); 
+    try {
+      
+        // create database Ren collection for Vehicles
+        const vehiclesCollection = client.db('rentWheels-BD').collection('vehicles')
+
+
+
+
+
+
+
+        
+
+        // Connect the client to the server	(optional starting in v4.7)
+        await client.connect();
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully RentWheels-BD connected to MongoDB!"); 
+
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
