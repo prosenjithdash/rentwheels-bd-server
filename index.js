fcd852ef0248c0c63fbb31179129abb399cd6690
Prosenjith Dash Pappu <prosenjith.edu@gmail.com>
2025-11-05 04:16:46 +0600

    useRole server api create done

--- index.js
+++ index.js
@@ -96,6 +96,13 @@ async function run() {
         const result = await usersCollection.find().toArray()
          res.send (result)
       })
+
+      // get a user info by email from db
+      app.get('/user/:email', async (req, res) => {
+        const email = req.params.email
+        const result = await usersCollection.findOne({email})
+        res.send(result)
+      })
  
               
       // VEHICLES PART
