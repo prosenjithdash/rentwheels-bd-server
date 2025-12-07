5d34119128e27c76fb9aba2351e4521f16468d7c
Prosenjith Dash Pappu <prosenjith.edu@gmail.com>
2025-12-07 22:18:02 +0600

    Apply stripe post api

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const jwt = require('jsonwebtoken');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const cookieParser = require('cookie-parser');
 const app = express();
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
+
 const port = process.env.PORT || 8000;
 
 // 🧩 Middleware
@@ -102,6 +104,30 @@ async function run() {
         res.status(500).send(err)
       }
     })
+      
+      
+      
+      // create-payment-intent
+      app.post("/create-payment-intent", verifyToken, async (req, res) => { 
+        const price = req.body.price;
+        const priceInCent = parseFloat(price) * 100
+
+        if (!price || priceInCent < 1) return;
+
+        // generate clientSecret
+        const {client_secret} = await stripe.paymentIntents.create({
+          amount: priceInCent,
+          currency: "usd",
+          // In the latest version of the API, specifying the `automatic_payment_methods` parameter is optional because Stripe enables its functionality by default.
+          automatic_payment_methods: {
+            enabled: true,
+          },
+        })
+
+        // send client secret as response
+        res.send({clientSecret: client_secret})
+
+      })
 
         // get all VEHICLES from database
         // app.get('/vehicles', async (req, res) => {
