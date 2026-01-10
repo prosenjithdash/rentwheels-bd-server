bfe5a4a4faf48f4e40fd099cc990c79a80e5fc71
Prosenjith Dash Pappu <prosenjith.edu@gmail.com>
2026-01-10 14:14:08 +0600

    Render Statistics API route create done

--- index.js
+++ index.js
@@ -428,6 +428,45 @@ async function run() {
       })
 
 
+      // Render Statistics API route
+      app.get('/render_stat', verifyToken, async (req, res) => {
+        const {email} = req.user
+        const bookingDetails = await bookingsCollection.find(
+          {'render.email': email},
+          {
+            projection: {
+            date: 1,
+            price: 1,
+
+          }
+        }).toArray()
+
+        const totalSales = bookingDetails.reduce((sum, booking) => sum + booking.price, 0)
+
+        const {timestamp} = await usersCollection.findOne({email},{projection:{timestamp:1}})
+
+        const chartData = bookingDetails.map(booking => {
+          const day = new Date(booking.date).getDate()
+          const month = new Date(booking.date).getMonth() + 1
+          const data = [`${day}/${month}`, booking?.price]
+          return data
+        })
+        chartData.unshift(['Day', 'Sales'])
+
+        console.log(chartData)
+        console.log(bookingDetails)
+
+        res.send(
+          {
+            totalBookings: bookingDetails.length,
+            totalSales,
+            chartData,
+            renderSince: timestamp
+          })
+      })
+
+
+
 
 
       // ✅ Get all vehicles added by a specific host (My Listings)
